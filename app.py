bcdc4e19c0c11d46b16faeecc18e90306abbf5ad
Abdullah alshaiabania <abdullahalshaiabani@Abdullahs-MacBook-Air.local>
2020-12-26 19:27:08 +0300

    added session

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, request, session, redirect, url_for
 from markupsafe import escape
 import random
 import requests
+from flask import render_template
 import json
 
 app=Flask(__name__)
+app.secret_key = "rps_key"
 
-global player1_score
-global player2_score
 
 
 def rock_paper(player1,player2):
@@ -15,40 +15,27 @@ def rock_paper(player1,player2):
     if (player1 =="rock" and player2=="scissor") or (player1=="scissor" and player2=="paper" ) or ( player1=="paper" and player2=="rock"):
         print("player 1 won this round")
         score=1
-    elif (player2 =="rock" and player1=="scissor") or ( player2=="scissor" and player1=="paper")or (player2=="paper" and player1=="rock"):
+    elif (player2 =="rock" and player1=="scissor") or ( player2=="scissor" and player1=="paper") or (player2=="paper" and player1=="rock"):
         print("player 2 won this round")
         score=-1
     else:
         score=0
     return score
 
-@app.route('/restart')
-def play_restart():
-    global player1_score,player2_score
-    player1_score=0
-    player2_score=0
-    return "Score has been reset"
-
-
-
-
-
-
-@app.route('/')
-def root():
-    return 'WORKING'
 
 
 dict_try={"0":"rock","1":"paper","2":"scissor"}
 
 @app.route('/play/<val>')
-def play_game(val):
-    global player1_score,player2_score
-
+def play_game(val):    
+    print("Call for a try")
+    if 'user_name' not in session:
+        return redirect(url_for("login"))
     # player 1 is computer  
     # player 2 is human
 
     # hand of computer
+
     player1=random.choice(["rock","paper","scissor"])
 
     #hand of human
@@ -58,32 +45,88 @@ def play_game(val):
     print(" Player 1(Computer) tried ",player1)
     print(" Player 2(Human) tried ",player2)
 
-    score=rock_paper(player1,player2)
-    if score==1:
-        player1_score+=1
-    elif score==-1:
-        player2_score+=1
-
-
     result_string=""
     result_string+="Player 1(Computer) tried "+player1
     result_string+="</br>Player 2(Human) tried "+player2
+
+
+    loc_score=rock_paper(player1,player2)
+    print("score for ",player1,player2 ,"is ",str(loc_score))
+
+    if loc_score==1:
+        session["player1_score"]+=1
+    elif loc_score==-1:
+        session["player2_score"]+=1
+
+    print("Current score for player 1",str(session["player1_score"]))
+    print("Current score for player 2",str(session["player2_score"]))
+
+
     
 
-    result_string+="</br>player1(computer score) = "+str(player1_score)
-    result_string+="</br>player2(human score) = "+str(player2_score)
-    if player1_score ==3 :
+    result_string+="</br>player1(computer score) = "+str(session["player1_score"])
+    result_string+="</br>player2(human score) = "+str(session["player2_score"])
+    if session["player1_score"] ==3 :
         result_string+="</br> congratualtion computer has won"
-        player1_score=0
-        player2_score=0
-    elif player2_score ==3:
-         player2_score=0
-         player1_score=0
+        session["player1_score"]=0
+        session["player2_score"]=0
+    elif session["player2_score"] ==3:
+         session["player2_score"]=0
+         session["player1_score"]=0
          result_string+="</br> congratualtion you have won"
         
     return result_string
 
-    
+
+
+@app.route('/restart')
+def new_game():
+    print("Restarting")  
+    session.pop('player1_score',None)
+    session.pop('player2_score',None)
+    session["player1_score"]=0
+    session["player2_score"]=0
+    print("Scores are ",session["player1_score"],session["player2_score"])
+    return "Score has been reset"
+
+
+
+
+
+
+@app.route('/')
+def root():
+    return render_template("index.html")
+
+@app.route('/login')
+def login():
+    if 'user_name' in session:
+        return str(session["user_name"])+" Logged in already"
+    else:
+        return render_template("login.html")  
+    # return 'WORKING'
+
+@app.route('/logout')
+def logout():
+    session.pop('user_name',None)
+    session.pop('player1_score',None)
+    session.pop('player2_score',None)
+    return redirect(url_for("login"))
+
+@app.route('/postlogin',methods=['POST'])
+def postlogin():
+    print("At logging in",request)
+    user_name=request.form['username']
+    session["user_name"] = user_name
+    session['player1_score']=0 
+    session['player2_score']=0 
+    print("Got username ",user_name)
+
+
+    return str(session["user_name"])+" Logged in succesfully"
+
+
+
 
 
 @app.route('/user/<username>')
